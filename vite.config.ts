e87f64d8ffadccbb358d1979032a16232661a1be
Kaipeng Yu <kaipeng@umich.edu>
2025-11-24 13:05:46 -0500

    update vite setting

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    const env = loadEnv(mode, process.cwd(), '');
     return {
       server: {
         port: 3000,
@@ -11,9 +11,9 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.WEATHER_API_KEY': JSON.stringify(env.WEATHER_API_KEY)
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || process.env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || process.env.GEMINI_API_KEY),
+        'process.env.WEATHER_API_KEY': JSON.stringify(env.WEATHER_API_KEY || process.env.WEATHER_API_KEY || env.VITE_WEATHER_API_KEY || process.env.VITE_WEATHER_API_KEY)
       },
       envPrefix: 'VITE_',
       resolve: {
